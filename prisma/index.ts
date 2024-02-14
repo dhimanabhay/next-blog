07039fecd257318bf2f542f75a2523f982b6bf88
adhiman <adhiman@sfu.ca>
2024-02-13 22:32:13 -0800

    fi

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,4 +1,4 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 // let prisma: PrismaClient;
 // declare global {
@@ -9,7 +9,7 @@
 //   }
 // }
 
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
 let prisma: PrismaClient;
 
